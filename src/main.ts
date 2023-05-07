069136946084f1db63f01bb0da0434f15e3c2293
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2023-05-07 20:16:31 +0800

    fix: Escape special characters in the page name.

--- src/main.ts
+++ src/main.ts
@@ -54,10 +54,10 @@ async function main() {
     }
 
     // trim surrounding [[]] from target in case it is a page reference
-    // and escape (, `, ', ), =, :, ., ~, ?, ", %
+    // and escape (, ), =, ?, "
     const targetPage = target
       .replace(/^(\[\[)(.*)(\]\])$/, "$2")
-      .replace(/([`'()=:~?".%])/g, "\\$1")
+      .replace(/([()=?"])/g, "\\$1")
 
     // split properties by ":", remove any empty entries.
     const propertyNames = propertyNameList.split(":").map((prop) => prop.trim()).filter(prop => prop !== "")
